d2b87238758601e68f898dc6374b7fed7d382379
farukumarx64 <f.umaridris.mail@gmail.com>
2023-10-15 09:37:13 +0100

    Navbar component created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import '../styles/NavBar.css'
+
+function NavBar() {
+  return (
+    <>
+     <button className="nav-button">About</button> 
+     <button className="nav-button">Education</button>
+     <button className="nav-button">Experience</button>
+    </>
+  )
+}
+export default NavBar;
---
+++ src/components/PersonalInfo.jsx
@@ -0,0 +1,11 @@
+function PersonalInfo () {
+  return (
+    <>
+      <h1>Personal Info</h1>
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Email" />
+      <input type="tel" placeholder="Phone Number" />
+    </>
+  )
+}
+export default PersonalInfo;
